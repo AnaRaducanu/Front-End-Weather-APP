71bfba530fdc143f058e7e25947aeb8f9c8c3251
Ana Raducanu <danuta.anna@yahoo.com>
2024-04-14 11:59:00 +0100

    1st passing unit test

--- my-weather-app/src/App.test.tsx
+++ my-weather-app/src/App.test.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import HelloWorld from "./components/HelloWorld";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the "Hello World" message', () => {
+  render(<HelloWorld />);
+  const helloWorldText = screen.getByText("Hello, World!");
+  expect(helloWorldText).toBeInTheDocument();
 });
--- my-weather-app/src/App.tsx
+++ my-weather-app/src/App.tsx
@@ -1,8 +1,11 @@
+import React from "react";
+import HelloWorld from "./components/HelloWorld";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello World!</header>
-    </div>
+    <>
+      <HelloWorld />
+    </>
   );
 }
 
